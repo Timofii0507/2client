95322f746e7bc5be0538e20d56a234efc9a0d8c1
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-08-26 16:45:08 +0300

    hm

--- 2client/MainWindow.xaml.cs
+++ 2client/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +14,48 @@ using System.Windows.Shapes;
 
 namespace _2client
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private UdpClient udpClient;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void SendQuery_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var serverIp = ServerIpTextBox.Text;
+                var products = ProductListTextBox.Text;
+                var endpoint = new IPEndPoint(IPAddress.Parse(serverIp), 11000);
+
+                using (udpClient = new UdpClient())
+                {
+                    byte[] queryBytes = Encoding.UTF8.GetBytes(products);
+                    await udpClient.SendAsync(queryBytes, queryBytes.Length, endpoint);
+
+                    var recipeResult = await udpClient.ReceiveAsync();
+                    string recipe = Encoding.UTF8.GetString(recipeResult.Buffer);
+                    RecipeTextBlock.Text = recipe;
+
+                    var imageResult = await udpClient.ReceiveAsync();
+                    using (var stream = new MemoryStream(imageResult.Buffer))
+                    {
+                        var image = new BitmapImage();
+                        stream.Position = 0;
+                        image.BeginInit();
+                        image.StreamSource = stream;
+                        image.EndInit();
+                        RecipeImage.Source = image;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
         }
     }
 }
